930accdfbd0daa3b3a5835cbfe88bdca5faa52eb
Lily <133771406+lilylilo@users.noreply.github.com>
2024-09-07 13:10:26 -0300

    5

---
+++ main (4).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+  int x=5;
+  int y = 10;
+  printf("\n Valor guardado em x: %d",x);
+  printf("\n Valor guardado em y: %d",y);
+  printf("\n Endereco de x: %x",&x);
+  printf("\n Endereco de y: %x",&y);
+}

fcc1aa182eeb9a6c7cab98722665008a22cd76c0
Lily <133771406+lilylilo@users.noreply.github.com>
2024-09-07 13:13:33 -0300

    7

---
+++ main (6).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+  {
+    int num;
+    printf("digite um número: ");
+    scanf("%od",&num);
+    if (num%3==0);
+      printf("O número é múltiplo de 3!");
+    
+    return 0;
+  }
